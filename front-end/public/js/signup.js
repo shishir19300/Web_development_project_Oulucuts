1692876d07d3916378f7b6ecd5c112d59d0cf30c
shishir19300 <shishirlamsal2004@gmail.com>
2026-04-02 00:10:47 +0300

    fixed the signup html

---
+++ front-end/public/js/signup.js
@@ -0,0 +1,43 @@
+const signupForm = document.getElementById('signup-form');
+const errorMsg   = document.getElementById('error-msg');
+const successMsg = document.getElementById('success-msg');
+
+signupForm.addEventListener('submit', async (event) => {
+    
+    event.preventDefault();
+const username = document.getElementById('signup-username').value.trim(); 
+const password = document.getElementById('signup-password').value;
+const age      = document.getElementById('signup-age').value;
+const errorMsg   = document.getElementById('error-msg');
+const successMsg = document.getElementById('success-msg');
+const genderElement = document.querySelector('input[name="gender"]:checked');
+    const gender = genderElement ? genderElement.value : '';
+
+errorMsg.style.display   = 'none';
+successMsg.style.display = 'none';
+
+if(!username || !password || !gender || !age){
+    errorMsg.textContent = 'Fill all the fields';
+    errorMsg.style.display = 'block';
+    return
+}
+try {
+    const response = await fetch('http://localhost:3000/api/auth/register', {
+      method:  'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body:    JSON.stringify({name:username, username, password, gender, age: parseInt(age) })
+    });
+    const data = await response.json();
+
+    if(response.ok) { 
+    successMsg.textContent   = '✅ Account created! Redirecting...';
+    successMsg.style.display = 'block'; 
+    } else{
+        errorMsg.textContent = data.error;
+        errorMsg.style.display = 'block';
+    }
+    } catch (err) {
+    errorMsg.textContent   = ' Server is offline!';
+    errorMsg.style.display = 'block';
+  }
+});
