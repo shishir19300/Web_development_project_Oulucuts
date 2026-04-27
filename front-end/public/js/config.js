5ecd328544ab850134d86c557397f17f1b5e8fa6
shishir19300 <shishirlamsal2004@gmail.com>
2026-04-27 23:45:28 +0300

    fixed booking

--- front-end/public/js/config.js
+++ front-end/public/js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   API_BASE: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-    ? 'http://localhost:3000'
+    ? 'http://127.0.0.1:3000'
     : 'https://oulucuts-back-end.onrender.com'
 };
